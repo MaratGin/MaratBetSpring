8b76838b41c01b01fc0b8ff8e46b2b580acf95b4
MaratGin <40231423+MaratGin@users.noreply.github.com>
2022-05-10 21:26:35 +0300

    checkout develop

--- src/main/java/project/configs/WebConfig.java
+++ src/main/java/project/configs/WebConfig.java
@@ -16,6 +16,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 @EnableWebMvc
 
+
+
+
 public class WebConfig extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
